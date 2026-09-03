66d193c3d83c533f1d02c19052d0aaccebf9744b
larry <larryhe360@gmail.com>
2026-09-03 19:38:06 -0400

    dracula theme

--- home_modules/vscode.nix
+++ home_modules/vscode.nix
@@ -8,6 +8,7 @@
     profiles.default.extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
       ms-python.python
+      dracula-theme.theme-dracula
     ];
 
     profiles.default.userSettings = {
@@ -15,6 +16,7 @@
       "editor.tabSize" = 4;
       "files.trimTrailingWhitespace" = true;
       "nix.enableLanguageServer" = true;
+      "workbench.colorTheme" = "Dracula Theme";
     };
   };
 }
