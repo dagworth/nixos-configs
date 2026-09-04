0a65dfdf752797f6afcdc9d9f109f3158ac23e61
larry <larryhe360@gmail.com>
2026-09-04 18:13:33 -0400

    ff

--- home_modules/dotfiles.nix
+++ home_modules/dotfiles.nix
@@ -7,4 +7,12 @@
     "quickshell".source = ../dotfiles/quickshell;
     "kitty".source = ../dotfiles/kitty;
   };
+
+  programs.firefox.profiles.default = {
+    isDefault = true;
+    userChrome = ../dotfiles/firefox/chrome/userChrome.css;
+    settings = {
+      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    };
+  };
 }
