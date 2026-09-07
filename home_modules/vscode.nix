e13f8b05fab5a8ab7cdd1196726a444af2deb031
larry <larryhe360@gmail.com>
2026-09-07 05:23:14 -0400

    more vscode stuff

--- home_modules/vscode.nix
+++ home_modules/vscode.nix
@@ -7,16 +7,22 @@
 
     profiles.default.extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
-      ms-python.python
       dracula-theme.theme-dracula
+      vscode-icons-team.vscode-icons
     ];
 
     profiles.default.userSettings = {
       "editor.fontSize" = 16;
       "editor.tabSize" = 4;
       "files.trimTrailingWhitespace" = true;
-      "nix.enableLanguageServer" = true;
+      "workbench.iconTheme" = "vscode-icons";
+      "workbench.colorCustomizations" = {
+        "sideBar.foreground" = "#b4a3c1";
+      };
       "workbench.colorTheme" = "Dracula Theme";
+      "workbench.tree.indent" = 16;
+      "workbench.tree.renderIndentGuides" = "always";
+      "workbench.activityBar.location" = "bottom";
     };
   };
 }
