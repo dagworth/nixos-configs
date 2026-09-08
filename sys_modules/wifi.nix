2fc3adebc263fe982fa8c17f110ba4723dfc6746
larry <larryhe360@gmail.com>
2026-09-08 17:07:57 -0400

    wifi

---
+++ sys_modules/wifi.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+let
+  wifi = import ./secrets.nix;
+in
+{
+ networking.networkmanager.ensureProfiles.profiles.${wifi.ssid} = {
+   connection = {
+     id = wifi.ssid;
+     type = "wifi";
+     autoconnect = true;
+   };
+   wifi = {
+     mode = "infrastructure";
+     ssid = wifi.ssid;
+   };
+   wifi-security = {
+     key-mgmt = "wpa-eap";
+   };
+   "802-1x" = {
+     eap = "ttls;";
+     phase2-auth = "pap";
+     identity = wifi.identity;
+     password = wifi.password;
+     ca-cert = wifi.cert;
+     domain-suffix-match = wifi.domain;
+   };
+   ipv4.method = "auto";
+   ipv6.method = "auto";
+ };
+}
